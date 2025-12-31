d743d03cf27501fd0c5272ba5c6f9d111438cbd1
Praveen Nalakurthi <praveen.nalakurthi@gmail.com>
2025-12-31 16:46:23 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,10 @@ export default defineConfig({
   server: {
     port: 5178,
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+  publicDir: "public",
 })
